4a1738c4abc85c473657c8ddc9c4ef9c5745c12a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-11-17 23:57:54 +0100

    refactor: review fetch_repos_status.js

--- fetch_repos_status.js
+++ fetch_repos_status.js
@@ -3,14 +3,17 @@
 require("dotenv").config();
 require("make-promises-safe");
 
+// Require Node.js Dependencies
+const { writeFile, appendFile } = require("fs").promises;
+
 // Require Third-party Dependencies
 const { fetch } = require("fetch-github-repositories");
 const { get } = require("httpie");
-const fs = require("fs");
 
 // CONSTANTS
 const GITHUB_ORGA = "SlimIO";
 const GITHUB_TOKEN = process.env.GIT_TOKEN;
+const FILE_NAME = "project_status.md";
 
 async function getTomlType(repoName) {
     try {
@@ -30,9 +33,6 @@ async function getTomlType(repoName) {
 }
 
 async function main() {
-
-    const filename = "project_status.md";
-
     console.time("fetchRepos");
     const repos = await fetch(GITHUB_ORGA, {
         kind: "orgs",
@@ -40,52 +40,45 @@ async function main() {
         agent: GITHUB_ORGA
     });
     console.timeEnd("fetchRepos");
+    console.log(repos.length);
 
-    const projectsInformation = [];
-    const kinds = new Map([]);
-    
+    const kinds = new Map();
+
     for (const { name, description, html_url, archived } of repos) {
+        if (archived) {
+            continue;
+        }
 
-	const repo_name = html_url.replace("https://github.com/" + GITHUB_ORGA + "/", "");    
-	const package_file = "https://raw.githubusercontent.com/" + GITHUB_ORGA + "/" + repo_name + "/master/package.json";
-	const project_version = `![version](https://img.shields.io/badge/dynamic/json.svg?url=${package_file}&query=$.version&label=Version)`;
-	const node_version = `![version](https://img.shields.io/badge/dynamic/json.svg?url=${package_file}&query=$.engines.node&label=Node)`;
-	const dependencies = `![dependencies](https://img.shields.io/david/SlimIO/${repo_name})`;
+        const repo_name = html_url.replace("https://github.com/" + GITHUB_ORGA + "/", "");
+        const type = await getTomlType(repo_name);
+        if (type === null) {
+            continue;
+        }
 
-	const repo = {name, description, project_version, node_version, dependencies};
-	
-	if (archived) {
-		continue;
-	}
-	
-	const type = await getTomlType(repo_name);
-	if (type === null) {
-		continue; 
-	}
+        const package_file = "https://raw.githubusercontent.com/" + GITHUB_ORGA + "/" + repo_name + "/master/package.json";
+        const project_version = `![version](https://img.shields.io/badge/dynamic/json.svg?url=${package_file}&query=$.version&label=Version)`;
+        const node_version = `![version](https://img.shields.io/badge/dynamic/json.svg?url=${package_file}&query=$.engines.node&label=Node)`;
+        const dependencies = `![dependencies](https://img.shields.io/david/SlimIO/${repo_name})`;
 
-	if (kinds.has(type)) {
-		kinds.get(type).push(repo);
-	} else {
-		kinds.set(type, [repo]);	
-	}
+        const repo = { name, description, project_version, node_version, dependencies };
+        if (kinds.has(type)) {
+            kinds.get(type).push(repo);
+        } else {
+            kinds.set(type, [repo]);
+        }
     }
 
     // Creation du fichier
-    fs.writeFile(filename, "# Module Status\n\n", function (err) {
-        if (err) throw err;
-        console.log(`file ${filename} create`);
-    });
+    await writeFile(FILE_NAME, "# Module Status\n\n");
+    console.log(`file ${FILE_NAME} create`);
 
-    for (const [type, repos] of kinds.entries()){
-	fs.appendFile(filename, "## " + type + "\n\nNom | Description | Version | Node | Dependencies\n :-: | :-: | :-: | :-: | :-:\n", function (err) {
-            if (err) throw err;
-        });
+    for (const [type, repos] of kinds.entries()) {
+        await appendFile(FILE_NAME, "\n## " + type + "\n\nNom | Description | Version | Node | Dependencies\n --- | ---\ | :-: | :-: | :-:\n");
 
-    	for (const {name, description, project_version, node_version, dependencies} of repos) {
-	    fs.appendFile(filename, `**${name}** | ${description} | ${project_version} | ${node_version} | ${dependencies}\n`, function (err) {
-                if (err) throw err;
-            });
-	}
+        for (const { name, description, project_version, node_version, dependencies } of repos) {
+            const updDesc = description.length > 50 ? `${description.slice(0, 47)}...` : description;
+            await appendFile(FILE_NAME, `**${name}** | ${updDesc} | ${project_version} | ${node_version} | ${dependencies}\n`);
+        }
     }
 }
 main().catch(console.error);
